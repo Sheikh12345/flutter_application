afc881081643486333be58c8fd5af034929bf612
kashif sheikh <kashifhafeez033@gmail.com>
2021-06-21 16:53:04 +0500

    login system working fine now

--- lib/app/modules/auth/bindings/auth_binding.dart
+++ lib/app/modules/auth/bindings/auth_binding.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:home_services/app/modules/home/controllers/home_controller.dart';
+import 'package:home_services/app/modules/root/controllers/root_controller.dart';
 
 import '../controllers/auth_controller.dart';
 
@@ -15,5 +16,8 @@ class AuthBinding extends Bindings {
         ()=> HomeController(),
       fenix: true
     );
+
+    Get.lazyPut<RootController>(() => RootController(),
+    fenix: true);
   }
 }
